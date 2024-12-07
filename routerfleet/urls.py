c9c887d289c6a5f73caf4f348df8d791a94c9e87
abnerdavi <abnerdavi123@gmail.com>
2024-12-07 18:22:06 -0300

    correção de exebicao de pagina teste

--- routerfleet/urls.py
+++ routerfleet/urls.py
@@ -63,5 +63,5 @@ urlpatterns = [
     path('message_center/manage_settings/', view_manage_message_settings, name='manage_message_settings'),
     path('message_center/manage_channel/', view_manage_message_channel, name='manage_message_channel'),
     path('message_center/message_history/', view_message_history, name='message_history'),
-    path('labautomation/hello_world/', view_message_history, name='hello_world'),
+    path('labautomation/hello_world/', hello_world, name='hello_world'),
 ]
